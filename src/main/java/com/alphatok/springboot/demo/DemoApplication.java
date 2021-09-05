5b28aa07e2e3be0b440313ee50a8ce0f4931c390
yang <yangtzerong@hotmail.com>
2021-09-05 16:48:00 +0800

    init mysql

--- src/main/java/com/alphatok/springboot/demo/DemoApplication.java
+++ src/main/java/com/alphatok/springboot/demo/DemoApplication.java
@@ -2,10 +2,12 @@ package com.alphatok.springboot.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	@Transactional
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
